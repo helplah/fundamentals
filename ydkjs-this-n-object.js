0a324d42f1c35819c058261e196a30b2f5fa4ac2
Jen <30688276+helplah@users.noreply.github.com>
2018-07-28 14:44:27 +0800

    this n object updated

--- ydkjs-this-n-object.js
+++ ydkjs-this-n-object.js
@@ -1,3 +1,4 @@
+/* chp 1
 function foo(num) {
   console.log( "foo: " + num );
   // keep track of how many times `foo` is called
@@ -19,3 +20,85 @@ for (i=0; i<10; i++) {
 // foo: 9
 // how many times was `foo` called?
 console.log('foo.count:', foo.count ); // 0 -- WTF?
+*/
+
+
+
+// Chp 2 - inspect call-site and determine which of the four rules applies
+// Default Binding
+/*
+function foo() {
+  console.log(this.a);
+}
+var a = 2;
+foo(); // 2
+
+function foo() {
+  'use strict';
+  console.log(this.a);
+}
+var a = 2;
+foo(); // TypeError: this is undefined 
+*/
+
+
+/*
+// Implicit Binding
+function foo() {
+  console.log(this.a);
+}
+
+var obj = {
+  a: 2,
+  foo: foo
+}
+
+obj.foo();
+
+// only the top/last level of an object property reference chain matters to the call site
+function foo() {
+  console.log(this.a);
+}
+
+var obj2 = {
+  a: 42,
+  foo: foo
+}
+
+var obj1 = {
+  a: 2,
+  obj2: obj2
+}
+
+obj1.obj2.foo();
+*/
+
+
+// Explicit Binding
+/*
+function foo() {
+  console.log(this.a);
+}
+
+var obj = {
+  a: 2
+}
+
+foo.call(obj); 
+*/
+
+
+function foo() {
+  console.log(this.a);
+}
+
+var obj = {
+  a: 2
+};
+
+var bar = function() {
+  foo.call(obj);
+};
+
+bar(); // 2
+setTimeout(bar, 100); // 2 
